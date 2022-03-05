58fff5f1ff702d0ac96a553d02f8267c6dfdf619
onyto <onyto@tutanota.com>
2022-03-05 23:06:45 +0100

    Add a button that deletes the grid

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ createGrid(16, 16);
 const squares = document.querySelectorAll('.square');
 squares.forEach(square => square.addEventListener('mouseover', (colorSquare)));
 
+const button = document.querySelector('button');
+button.onclick = () => clearGrid();
+
 // Create a grid with X amount of rows that have X amount of squares in them
 function createGrid(rowNum, squareNum) {
   const container = document.querySelector('#container');
@@ -28,4 +31,9 @@ function createGrid(rowNum, squareNum) {
 
 function colorSquare() {
   this.classList.add('color');
+}
+
+function clearGrid() {
+  const rows = document.querySelectorAll('.row');
+  rows.forEach(row => row.remove());
 }
